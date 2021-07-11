47e1540a30a29b0fbbebf475b06e858856e57190
Hossain Mohammad Farhad <farhad@primetechbd.com>
2021-07-11 20:36:30 +0600

    Add project files.

---
+++ VoiceRecognition/Models/VoiceSearchViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VoiceRecognition.Models
+{
+    public class VoiceSearchViewModel
+    {
+        public int MyProperty { get; set; }
+    }
+}
